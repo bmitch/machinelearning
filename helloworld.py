5ca7fa8543fc4c100fd799b78ee5fb62459fb269
Bill Mitchell <wkmitch@gmail.com>
2016-04-14 02:20:33 +0100

    First commit

---
+++ helloworld.py
@@ -0,0 +1,13 @@
+from sklearn import tree
+
+# 0 = bumpy, 1 = smooth
+features = [[140, 1], [130, 1], [150, 0], [170, 0]]
+
+# 0 = Apple, 1 = Orange
+labels = [0, 0, 1, 1]
+
+clf = tree.DecisionTreeClassifier()
+
+clf = clf.fit(features, labels)
+
+print clf.predict([[150, 0]])
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Machine Learning #
+
+Following tutorials from Google Developers
+
+* Lesson 01 - Hello World https://youtu.be/cKxRvEZd3Mw
